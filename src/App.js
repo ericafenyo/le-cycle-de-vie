75c00deecaae14302b84d8247007181eadf325df
ericafenyo <ericafenyo@yahoo.com>
2019-04-15 12:22:10 +0200

    code update

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const Form = ({ handleChange, handleSubmit, title, value }) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="input-title">{title}</label>
+      <input
+        type="text"
+        id="input-title"
+        onChange={handleChange}
+        value={value}
+      />
+      <input type="submit" />
+    </form>
+  );
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: "Title",
+      globalTitle: "Global Title"
+    }
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    this.setState({ title: value })
+  }
+
+  handleSubmit = (event) => {
+    const title = this.state.title
+
+    if (/\S/.test(title)) {
+      console.log("Formulaire rendu");
+      this.setState({globalTitle: `Mon formulaire - ${title}`})
+    }else{
+      alert("Input field can't be empty")
+    }
+    event.preventDefault()
+  }
+
+  componentDidUpdate(){
+    console.log("Titre changé");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="">
+          <h4>{this.state.globalTitle}</h4>
+          <Form
+            title={this.state.title}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            value={this.state.title}
+          />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
